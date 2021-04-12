18c8ed3e1bc9c4f79305c1b77d8a5f8ab9b727fe
shi meng <shimengfox@foxmail.com>
2021-04-12 22:06:06 +0800

    电话号码的字母组合

--- backtrace/letterCombinations.cpp
+++ backtrace/letterCombinations.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+vector<string> table {"", "", "abc", "def",
+                      "ghi","jkl","mno",
+                      "pqrs","tuv","wxyz"};
+
 /**
  * 给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
  * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
@@ -14,11 +18,6 @@ using namespace std;
  * @param digits 数字按键
  * @return 所有可能的字符
  */
-
-vector<string> table {"", "", "abc", "def",
-                      "ghi","jkl","mno",
-                      "pqrs","tuv","wxyz"};
-
 void dfs(string &digits, int pos, string str, vector<string> &res) {
     if (pos == digits.length()) {
         res.push_back(str);
