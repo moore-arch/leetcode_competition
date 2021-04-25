c4fc6b311411a0f8725f5d45d07c59b30134f45a
terminal <shimenghust.gmail>
2021-04-25 19:41:10 +0800

    add increasingBTS

---
+++ daily/increasingBST.cpp
@@ -0,0 +1,17 @@
+#include "common.h"
+#include "TreeNode.h"
+
+using namespace std;
+
+/**
+ * @brief 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ * 
+ * @param root 
+ * @return TreeNode* 
+ */
+TreeNode* increasingBST(TreeNode* root) {
+
+}
+int main() {
+	return 0;
+}
