5b50d2872a9b973615e7eab32719c3e0d4c5ee35
shi meng <shimengfox@foxmail.com>
2021-05-15 21:57:05 +0800

    add tree tools

--- headers/TreeNode.h
+++ headers/TreeNode.h
@@ -13,13 +13,34 @@ public:
     TreeNode *right;
     int val;
 
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-
-    static TreeNode* buildTree(const vector<int> &record) {
-
-    }
+//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//
+//    // Encodes a tree to a single string.
+//    static string serialize(TreeNode* root) {
+//
+//    }
+//
+//    // Decodes your encoded data to tree.
+//    static TreeNode* deserialize(string data) {
+//
+//    }
+//
+//    static TreeNode* buildTree(const vector<int> &nodes) {
+//        if (nodes.empty()) {
+//            return nullptr;
+//        }
+//        queue<TreeNode*> record;
+//        auto *root = new TreeNode(nodes[0]);
+//        record.push(root);
+//        for (int i = 1; i < record.size(); ++i) {
+//
+//        }
+//        for (const int &item : nodes) {
+//
+//        }
+//    }
 };
 
 #endif //LEETCODE_COMPETITION_TREENODE_H
