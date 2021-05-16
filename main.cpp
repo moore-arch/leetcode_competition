bc3e329a8b3becd5d6c95fe8a873f5a7c35718d7
shi meng <shimengfox@foxmail.com>
2021-05-16 15:18:17 +0800

    添加编译器检测

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <cstdlib>
+
+std::string ver_string(int a, int b, int c) {
+    std::ostringstream t;
+    t << a << '.' << b << '.' << c;
+    return t.str();
+}
+
+std::string true_cxx =
+#ifdef __clang__
+        "clang++";
+#else
+        "g++";
+#endif
+
+std::string true_cxx_ver =
+#ifdef __clang__
+        ver_string(__clang_major__, __clang_minor__, __clang_patchlevel__);
+#else
+        ver_string(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+#endif
+
+void showVersion() {
+    std::cout << true_cxx << ": " << true_cxx_ver << std::endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    showVersion();
+
     return 0;
 }
